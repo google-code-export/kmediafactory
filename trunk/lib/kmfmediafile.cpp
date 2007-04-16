241ef123a99b12de3d7afc08e53a9153c4750af2
petri.damsten <petri.damsten@4bd9ec94-6b1d-0410-bfb7-312270ef6092>
2007-04-16 21:37:05 +0000

    backup

--- trunk/lib/kmfmediafile.cpp
+++ trunk/lib/kmfmediafile.cpp
@@ -18,7 +18,7 @@
 //   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //**************************************************************************
 
-#include "qmediafile.h"
+#include "kmfmediafile.h"
 #include "run.h"
 #include "kmftime.h"
 #include "kdebug.h"
