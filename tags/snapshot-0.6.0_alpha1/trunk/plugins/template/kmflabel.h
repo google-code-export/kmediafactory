c5ab112012bea2c2daded0b65656de1fb70abb28
petri.damsten <petri.damsten@4bd9ec94-6b1d-0410-bfb7-312270ef6092>
2007-07-13 12:32:33 +0000

    Tagging the 0.6.0_alpha1 snapshot.

---
+++ tags/snapshot-0.6.0_alpha1/trunk/plugins/template/kmflabel.h
@@ -0,0 +1,54 @@
+//**************************************************************************
+//   Copyright (C) 2004-2006 by Petri Damsten
+//   petri.damsten@iki.fi
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the
+//   Free Software Foundation, Inc.,
+//   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//**************************************************************************
+#ifndef KMFLABEL_H
+#define KMFLABEL_H
+
+#include "kmfwidget.h"
+#include <QFont>
+
+/**
+@author Petri Damsten
+*/
+class KMFLabel : public KMFWidget
+{
+    Q_OBJECT
+  public:
+    KMFLabel(QObject *parent = 0);
+    ~KMFLabel();
+
+    const QString& text() const { return m_text; };
+    void setText(const QString& text);
+    const QFont& font() const { return m_font; };
+    void setFont(const QFont& font) { m_font = font; };
+    virtual void fromXML(const QDomElement& element);
+    virtual int minimumPaintWidth() const;
+    virtual int minimumPaintHeight() const;
+    virtual void setProperty(const QString& name, QVariant value);
+
+  protected:
+    virtual void paintWidget(QImage& layer, bool shdw = false);
+    QString fitText(QString txt, int width);
+
+  private:
+    QString m_text;
+    QFont m_font;
+};
+
+#endif
---
+++ tags/snapshot-0.6.0_alpha1/trunk/src/kmfdbusinterface.h
@@ -0,0 +1,75 @@
+//**************************************************************************
+//   Copyright (C) 2004-2006 by Petri Damsten
+//   petri.damsten@iki.fi
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the
+//   Free Software Foundation, Inc.,
+//   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//**************************************************************************
+#ifndef KMFDCOPINTERFACE_H
+#define KMFDCOPINTERFACE_H
+
+#include <QObject>
+
+class KProgressDialog;
+
+/**
+	@author Petri Damsten <petri.damsten@iki.fi>
+*/
+class KMFDbusInterface : public QObject
+{
+  public:
+    KMFDbusInterface(QObject* parent);
+    virtual ~KMFDbusInterface();
+
+    virtual void openProject(QString file);
+    virtual void newProject();
+    virtual void quit();
+
+    virtual void setProjectTitle(const QString& title);
+    virtual void setProjectType(const QString& type);
+    virtual void setProjectDirectory(const QString& dir);
+    virtual QString projectTitle();
+    virtual QString projectType();
+    virtual QString projectDirectory(const QString& subdir);
+
+    virtual void addMediaObject(const QString& xml);
+    virtual void selectTemplate(const QString& xml);
+    virtual void selectOutput(const QString& xml);
+
+    virtual void start();
+
+    // Script helpers
+    virtual QString getOpenFileName (const QString &startDir,
+                                     const QString &filter,
+                                     const QString &caption);
+    virtual void debug(const QString &txt);
+    virtual int  message(const QString &caption, const QString &txt,
+                         int type);
+
+    // Progress dialog
+    virtual void progressDialog(const QString &caption, const QString &label,
+                                int maximum);
+    virtual void pdlgSetMaximum(int maximum);
+    virtual void pdlgSetValue(int value);
+    virtual void pdlgSetLabel(const QString &label);
+    virtual void pdlgShowCancelButton(bool show);
+    virtual bool pdlgWasCancelled();
+    virtual void pdlgClose();
+
+  private:
+    KProgressDialog* m_pdlg;
+};
+
+#endif
