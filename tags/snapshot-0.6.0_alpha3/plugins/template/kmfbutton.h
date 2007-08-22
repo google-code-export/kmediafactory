282a4f37feb0f5603cb40fb3c521a25a8536db41
petri.damsten <petri.damsten@4bd9ec94-6b1d-0410-bfb7-312270ef6092>
2007-08-22 14:49:29 +0000

    Tagging the 0.6.0_alpha3 snapshot.

---
+++ tags/snapshot-0.6.0_alpha3/plugins/template/kmfbutton.h
@@ -0,0 +1,93 @@
+//**************************************************************************
+//   Copyright (C) 2004-2006 by Petri Damsten
+//   petri.damsten@iki.fi
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the
+//   Free Software Foundation, Inc.,
+//   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//**************************************************************************
+#ifndef KMFBUTTON_H
+#define KMFBUTTON_H
+
+#include <kmfwidget.h>
+
+class QDomText;
+
+class KMFJump
+{
+  public:
+    enum { TS_NONE = -1, TS_VMGM = 0 };
+    KMFJump() :
+      m_titleset(TS_NONE), m_menu(-1),
+      m_title(-1), m_chapter(0) {};
+    KMFJump(int menu, int titleset = TS_NONE) { setMenu(menu, titleset); };
+
+    void setMenu(int menu, int titleset = TS_NONE)
+      { m_titleset = titleset; m_title = -1; m_chapter = 0; m_menu = menu; };
+    void setTitle(int chapter, int titleset = TS_NONE)
+      { m_titleset = titleset; m_title = 1; m_chapter = chapter;
+        m_menu = -1; };
+    int titleSet() { return m_titleset; };
+    int menu() { return m_menu; };
+    //int title() { return m_title; };
+    int chapter() { return m_chapter; };
+    const QString& command() const { return m_command; };
+    void setCommand(const QString& command) { m_command = command; };
+  private:
+    int m_titleset;
+    int m_menu;
+    int m_title;
+    int m_chapter;
+    QString m_command;
+};
+
+/**
+*/
+class KMFButton : public KMFWidget
+{
+    Q_OBJECT
+  public:
+    enum Direction { Up = 0, Right, Down, Left };
+
+    KMFButton(QObject *parent = 0);
+    ~KMFButton();
+
+    KMFButton* up() { return m_buttons[Up]; };
+    void setUp(KMFButton* up) { m_buttons[Up] = up; };
+    KMFButton* down() { return m_buttons[Down]; };
+    void setDown(KMFButton* down) { m_buttons[Down] = down; };
+    KMFButton* left() { return m_buttons[Left]; };
+    void setLeft(KMFButton* left) { m_buttons[Left] = left; };
+    KMFButton* right() { return m_buttons[Right]; };
+    void setRight(KMFButton* right) { m_buttons[Right] = right; };
+    const QString& direction(Direction dir) { return m_directions[dir]; };
+
+    KMFJump jump() { return m_jump; };
+    void setJump(KMFJump jump) { m_jump = jump; };
+
+    void fromXML(const QDomElement& element);
+    void writeDvdAuthorXml(QDomText& element, QString type);
+    bool parseJump(bool addPages = true);
+    void parseDirections();
+
+  private:
+    KMFButton* m_buttons[4];
+    QString m_directions[4];
+    KMFJump m_jump;
+    QString m_jumpS;
+
+    KMFButton* parseDirection(const QString& dir);
+};
+
+#endif
---
+++ tags/snapshot-0.6.0_alpha3/src/kmediafactory.h
@@ -0,0 +1,140 @@
+//**************************************************************************
+//   Copyright (C) 2004-2006 by Petri Damsten
+//   petri.damsten@iki.fi
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the
+//   Free Software Foundation, Inc.,
+//   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//**************************************************************************
+
+#ifndef _KMEDIAFACTORY_H_
+#define _KMEDIAFACTORY_H_
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <kxmlguiwindow.h>
+#include <kdirwatch.h>
+
+// Define this for the test menu item
+// #define KMF_TEST
+
+class MediaPage;
+class TemplatePage;
+class OutputPage;
+class KMFMediaInterface;
+class KUrl;
+class KToggleAction;
+class KPageWidget;
+class KMFNewStuff;
+class KPageWidgetItem;
+
+/**
+ * @short Application Main Window
+ * @author Petri Damsten <petri.damsten@iki.fi>
+ * @version 0.1
+ */
+class KMediaFactory : public KXmlGuiWindow
+{
+    Q_OBJECT
+    friend class KMFApplication;
+  public:
+    enum Page { Media = 0 , Template, Output };
+
+    /**
+     * Default Constructor
+     */
+    KMediaFactory();
+
+    /**
+     * Default Destructor
+     */
+    virtual ~KMediaFactory();
+
+    /**
+     * setup menu actions
+     */
+    void setupActions();
+    void enableUi(bool enabled);
+    void showPage(int page);
+
+    MediaPage *mediaPage;
+    TemplatePage *templatePage;
+    OutputPage *outputPage;
+
+  protected:
+    virtual bool queryClose();
+    /**
+     * Overridden virtuals for Qt drag 'n drop (XDND)
+     */
+    virtual void dragEnterEvent(QDragEnterEvent *event);
+    virtual void dropEvent(QDropEvent *event);
+
+    /**
+     * This function is called when it is time for the app to save its
+     * properties for session management purposes.
+     */
+    void saveProperties(KConfigGroup*);
+
+    /**
+     * This function is called when this app is restored.  The KConfig
+     * object points to the session management config file that was saved
+     * with @ref saveProperties
+     */
+    void readProperties(KConfigGroup*);
+
+  public slots:
+    void fileNew();
+    void quit();
+    void load(const KUrl&);
+
+  protected slots:
+    void initGUI();
+
+  private slots:
+    void fileOpen();
+    void projectOptions();
+    void newStuff();
+    void fileSave();
+    void fileSaveAs();
+    void itemDelete();
+    void execTool();
+    void optionsConfigureKeys();
+    void optionsConfigureToolbars();
+    void optionsPreferences();
+    void newToolbarConfig();
+    void optionsShowToolbar();
+    void updateToolsMenu();
+#ifdef KMF_TEST
+    //void test();
+#endif
+
+  private:
+    KToggleAction* m_toolbarAction;
+    KPageWidget* m_janus;
+    QWidget* m_janusIconList;
+    bool m_enabled;
+    KMFNewStuff* m_newStuffDlg;
+    KPageWidgetItem* m_mediaPageItem;
+    KPageWidgetItem* m_templatePageItem;
+    KPageWidgetItem* m_ouputPageItem;
+    KDirWatch m_toolsWatch;
+
+    void connectProject();
+    void resetGUI();
+    bool checkSaveProject();
+};
+
+#endif // _KMEDIAFACTORY_H_
