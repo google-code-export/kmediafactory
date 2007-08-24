3fb2bbd2d5d5e07e4b1e02cd4a589a738e323c0f
petri.damsten <petri.damsten@4bd9ec94-6b1d-0410-bfb7-312270ef6092>
2007-08-24 06:33:50 +0000

    new oxygen style icons

---
+++ trunk/icons/bitmap_icons.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SIZES=( 16 22 32 48 64 128 )
+
+for ICON in *.svgz; do
+  BASE=${ICON:5}
+  BASE=${BASE/svgz/png}
+  for SIZE in "${SIZES[@]}"; do
+    PNG="cr$SIZE-$BASE"
+    inkscape --without-gui --file=$ICON --export-png=$PNG \
+        --export-width=$SIZE --export-height=$SIZE
+  done
+done
