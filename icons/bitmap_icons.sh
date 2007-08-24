c5b18a7a7a46d2b632865dd2ce67ded0f8906f5a
petri.damsten <petri.damsten@4bd9ec94-6b1d-0410-bfb7-312270ef6092>
2007-08-24 06:58:09 +0000

    use oxygen names

--- icons/bitmap_icons.sh
+++ icons/bitmap_icons.sh
@@ -6,7 +6,7 @@ for ICON in *.svgz; do
   BASE=${ICON:5}
   BASE=${BASE/svgz/png}
   for SIZE in "${SIZES[@]}"; do
-    PNG="cr$SIZE-$BASE"
+    PNG="ox$SIZE-$BASE"
     inkscape --without-gui --file=$ICON --export-png=$PNG \
         --export-width=$SIZE --export-height=$SIZE
   done
