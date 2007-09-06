0a141081b29cebf75c5237bf1c2c2b19d137c1e2
petri.damsten <petri.damsten@4bd9ec94-6b1d-0410-bfb7-312270ef6092>
2007-09-06 11:35:12 +0000

    Tagging the 0.6.0_beta3 snapshot.

---
+++ tags/snapshot-0.6.0_beta3/plugins/slideshow/slideshowplugin.cpp
@@ -0,0 +1,162 @@
+//**************************************************************************
+//   Copyright (C) 2004-2006 by Petri Damsten
+//   petri.damsten@iki.fi
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the
+//   Free Software Foundation, Inc.,
+//   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//**************************************************************************
+#include "config.h"
+#include "slideshowplugin.h"
+#include "ui_slideshowconfig.h"
+#include "slideshowpluginsettings.h"
+#include "slideshowobject.h"
+#include <kmftools.h>
+#include <kactioncollection.h>
+#include <klocale.h>
+#include <kaboutdata.h>
+#include <kpluginfactory.h>
+#include <kdeversion.h>
+#include <kstandarddirs.h>
+#include <kapplication.h>
+#include <kfiledialog.h>
+#include <kicon.h>
+#include <KPluginLoader>
+
+static const KAboutData about("kmediafactory_slideshow", 0,
+                              ki18n("KMediaFactory Slideshow"), VERSION,
+                              ki18n("Slideshow plugin for KMediaFactory."),
+                              KAboutData::License_GPL,
+                              ki18n(COPYRIGHT), KLocalizedString(),
+                              HOMEPAGE, BUG_EMAIL);
+
+K_PLUGIN_FACTORY(SlideshowFactory, registerPlugin<SlideshowPlugin>();)
+K_EXPORT_PLUGIN(SlideshowFactory("kmediafactory_slideshow"))
+
+class SlideshowConfig : public QWidget, public Ui::SlideshowConfig
+{
+  public:
+    SlideshowConfig(QWidget* parent = 0) : QWidget(parent)
+    {
+      setupUi(this);
+    };
+};
+
+SlideshowPlugin::SlideshowPlugin(QObject *parent, const QVariantList&) :
+  KMF::Plugin(parent)
+{
+  setObjectName("KMFSlideshow");
+  setupActions();
+}
+
+QAction* SlideshowPlugin::setupActions()
+{
+  // Initialize GUI
+  setComponentData(SlideshowFactory::componentData());
+  // Add action for menu item
+  QAction* addSlideshowAction = new KAction(KIcon("kuickshow"),
+                                     i18n("Add Slideshow"), parent());
+  addSlideshowAction->setShortcut(Qt::CTRL + Qt::Key_W);
+  actionCollection()->addAction("slideshow", addSlideshowAction);
+  connect(addSlideshowAction, SIGNAL(triggered()), SLOT(slotAddSlideshow()));
+
+  setXMLFile("kmediafactory_slideshowui.rc");
+
+  uiInterface()->addMediaAction(addSlideshowAction);
+
+  return addSlideshowAction;
+}
+
+void SlideshowPlugin::init(const QString &type)
+{
+  kDebug() << k_funcinfo << type << endl;
+  deleteChildren();
+
+  QAction* action = actionCollection()->action("slideshow");
+  if(!action)
+    return;
+
+  if (type.left(3) == "DVD")
+  {
+
+    m_dvdslideshow = KStandardDirs::findExe("dvd-slideshow");
+    if(m_dvdslideshow.isEmpty())
+    {
+      action->setEnabled(false);
+    }
+    else
+    {
+      action->setEnabled(true);
+    }
+  }
+  else
+  {
+    action->setEnabled(false);
+  }
+}
+
+void SlideshowPlugin::slotAddSlideshow()
+{
+  QStringList pics = KFileDialog::getOpenFileNames(
+      KUrl("kfiledialog:///<AddSlideshow>"),
+      "*.jpg *.png *.pdf *.odp *.odt *.ods *.odx *.sxw *.sxc *.sxi \
+       *.ppt *.xls *.doc|Pictures, Presentations\n*.*|All files");
+
+  if(pics.count() > 0)
+  {
+    KMF::UiInterface *m = uiInterface();
+    SlideshowObject *sob;
+
+    sob = new SlideshowObject(this);
+    QFileInfo fi(pics[0]);
+    QDir dir(fi.absolutePath());
+
+    if(pics.count() == 1)
+      sob->setTitle(KMF::Tools::simple2Title(fi.baseName()));
+    else if(!dir.dirName().isEmpty())
+      sob->setTitle(KMF::Tools::simple2Title(dir.dirName()));
+    else
+      sob->setTitle(i18n("Slideshow"));
+    sob->addPics(pics);
+    if(sob->slides().count() > 0)
+      m->addMediaObject(sob);
+  }
+}
+
+KMF::MediaObject* SlideshowPlugin::createMediaObject(const QDomElement& element)
+{
+  KMF::MediaObject *mob = new SlideshowObject(this);
+  if(mob)
+    mob->fromXML(element);
+  return mob;
+}
+
+QStringList SlideshowPlugin::supportedProjectTypes()
+{
+  QStringList result;
+  result << "DVD-PAL" << "DVD-NTSC";
+  return result;
+}
+
+const KMF::ConfigPage* SlideshowPlugin::configPage() const
+{
+  KMF::ConfigPage* configPage = new KMF::ConfigPage;
+  configPage->page = new SlideshowConfig;
+  configPage->config = SlideshowPluginSettings::self();
+  configPage->itemName = i18n("Slideshow plugin");
+  configPage->pixmapName = "kuickshow";
+  return configPage;
+}
+
+#include "slideshowplugin.moc"
---
+++ tags/snapshot-0.6.0_beta3/src/mediapage.cpp
@@ -0,0 +1,100 @@
+//**************************************************************************
+//   Copyright (C) 2004-2006 by Petri Damsten
+//   petri.damsten@iki.fi
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the
+//   Free Software Foundation, Inc.,
+//   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//**************************************************************************
+#include "mediapage.h"
+#include "kmediafactory.h"
+#include "kmfapplication.h"
+#include "kmficonview.h"
+#include <kmftools.h>
+#include "sizewidget.h"
+#include <kxmlguifactory.h>
+#include <kactioncollection.h>
+#include <QPoint>
+#include <QMenu>
+#include <QTimer>
+
+MediaPage::MediaPage(QWidget *parent) :
+  QWidget(parent)
+{
+  setupUi(this);
+
+  mediaFiles->setSpacing(5);
+  mediaFiles->setItemDelegate(new KMFItemDelegate());
+  connect(mediaFiles, SIGNAL(customContextMenuRequested(const QPoint&)),
+          this, SLOT(contextMenuRequested(const QPoint&)));
+}
+
+MediaPage::~MediaPage()
+{
+}
+
+void MediaPage::projectInit()
+{
+  calculateSizes();
+  mediaFiles->setModel(kmfApp->project()->mediaObjects());
+}
+
+void MediaPage::mediaModified()
+{
+  calculateSizes();
+}
+
+void MediaPage::contextMenuRequested(const QPoint &pos)
+{
+  QModelIndex i = mediaFiles->indexAt(pos);
+
+  if(!kmfApp->project()->mediaObjects()->isValid(i))
+    return;
+
+  KMF::MediaObject* ob = kmfApp->project()->mediaObjects()->at(i);
+  KMediaFactory* mainWindow = kmfApp->mainWindow();
+  KXMLGUIFactory* factory = mainWindow->factory();
+
+  QList<QAction*> actions;
+  ob->actions(actions);
+  factory->plugActionList(mainWindow,
+      QString::fromLatin1("media_file_actionlist"), actions);
+  QWidget *w = factory->container("media_file_popup", mainWindow);
+  if(w)
+  {
+    QMenu* popup = static_cast<QMenu*>(w);
+    popup->exec(mediaFiles->viewport()->mapToGlobal(pos));
+  }
+  factory->unplugActionList(mainWindow, "media_file_actionlist");
+}
+
+void MediaPage::calculateSizes()
+{
+  uint64_t max = 4700372992LL;
+  uint64_t size = 0;
+
+  if(kmfApp->project())
+  {
+    QList<KMF::MediaObject*> mobs = kmfApp->project()->mediaObjects()->list();
+    KMF::MediaObject *mob;
+
+    foreach(mob, mobs)
+      size += mob->size();
+    size += mobs.size() * 200 * 1024; // Not very good estimate...
+  }
+  sizeWidget->setMax(max);
+  sizeWidget->setSize(size);
+}
+
+#include "mediapage.moc"
