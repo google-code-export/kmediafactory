ff5d2cc4caa880506ecc2ab1aeae4a6223884611
petri.damsten <petri.damsten@4bd9ec94-6b1d-0410-bfb7-312270ef6092>
2008-02-07 09:47:58 +0000

    Tagging the 0.6.0 release.

---
+++ tags/release-0.6.0/plugins/output/outputplugin.cpp
@@ -0,0 +1,157 @@
+//**************************************************************************
+//   Copyright (C) 2004-2006 by Petri Damsten
+//   petri.damsten@iki.fi
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the
+//   Free Software Foundation, Inc.,
+//   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//**************************************************************************
+#include "config.h"
+#include "outputplugin.h"
+#include "dvddirectoryobject.h"
+#include "dvdauthorobject.h"
+#include "k3bobject.h"
+#include <kactioncollection.h>
+#include <kdeversion.h>
+#include <kapplication.h>
+#include <kgenericfactory.h>
+#include <kaction.h>
+#include <kfiledialog.h>
+#include <kglobal.h>
+#include <kiconloader.h>
+#include <kaboutdata.h>
+#include <kstandarddirs.h>
+#include <kicon.h>
+#include <krun.h>
+#include <QRegExp>
+#include <QPixmap>
+
+#ifdef HAVE_LIBDVDREAD
+#include "dvdinfo.h"
+#endif
+
+static const KAboutData about("kmediafactory_output", 0,
+                              ki18n("KMediaFactory Output"), VERSION,
+                              ki18n("Output plugin for KMediaFactory."),
+                              KAboutData::License_GPL,
+                              ki18n(COPYRIGHT), KLocalizedString(),
+                              HOMEPAGE, BUG_EMAIL);
+
+K_PLUGIN_FACTORY(OutputFactory, registerPlugin<OutputPlugin>();)
+K_EXPORT_PLUGIN(OutputFactory("kmediafactory_output"))
+
+OutputPlugin::OutputPlugin(QObject *parent, const QVariantList&) :
+  KMF::Plugin(parent), addPreviewDVDXine(0), addPreviewDVDKaffeine(0)
+{
+  setObjectName("KMFOutput");
+  // Initialize GUI
+  setComponentData(OutputFactory::componentData());
+  setXMLFile("kmediafactory_outputui.rc");
+
+  m_xine = KStandardDirs::findExe("xine");
+  m_kaffeine = KStandardDirs::findExe("kaffeine");
+
+#ifdef HAVE_LIBDVDREAD
+  dvdInfo =new KAction(KIcon("zoom-original"), i18n("DVD Info"), parent);
+  dvdInfo->setShortcut(Qt::CTRL + Qt::Key_I);
+  actionCollection()->addAction("dvd_info", dvdInfo);
+  connect(dvdInfo, SIGNAL(triggered()), SLOT(slotDVDInfo()));
+#endif
+  if(!m_xine.isEmpty())
+  {
+    addPreviewDVDXine =new KAction(KIcon("xine"),
+                                   i18n("Preview DVD in Xine"), parent);
+    addPreviewDVDXine->setShortcut(Qt::CTRL + Qt::Key_X);
+    actionCollection()->addAction("preview_dvd_xine", addPreviewDVDXine);
+    connect(addPreviewDVDXine, SIGNAL(triggered()), SLOT(slotPreviewDVDXine()));
+  }
+  if(!m_kaffeine.isEmpty())
+  {
+    addPreviewDVDKaffeine =new KAction(KIcon("xine"),
+                                   i18n("Preview DVD in Kaffeine"), parent);
+    addPreviewDVDKaffeine->setShortcut(Qt::CTRL + Qt::Key_K);
+    actionCollection()->addAction("preview_dvd_kaffeine",
+                                  addPreviewDVDKaffeine);
+    connect(addPreviewDVDKaffeine, SIGNAL(triggered()),
+            SLOT(slotPreviewDVDKaffeine()));
+  }
+}
+
+void OutputPlugin::init(const QString &type)
+{
+  kDebug() << type;
+  deleteChildren();
+  if (type.left(3) == "DVD")
+  {
+    KMF::UiInterface *ui = uiInterface();
+    if(ui)
+    {
+      DvdAuthorObject *daob = new DvdAuthorObject(this);
+      ui->addOutputObject(daob);
+      DvdDirectoryObject *ddob = new DvdDirectoryObject(this);
+      ui->addOutputObject(ddob);
+      DvdAuthorObject *k3bob = new K3bObject(this);
+      ui->addOutputObject(k3bob);
+    }
+  }
+}
+
+void OutputPlugin::play(const QString& player)
+{
+  QString cmd;
+  QString projectDir = projectInterface()->projectDir();
+
+  if(player.isEmpty())
+  {
+    if(!m_xine.isEmpty())
+      cmd = m_xine;
+    else if(!m_kaffeine.isEmpty())
+      cmd = m_kaffeine;
+    else
+      return;
+  }
+  else
+    cmd = player;
+  cmd +=  " dvd:/" + projectDir + "DVD/VIDEO_TS";
+  KRun::runCommand(cmd, kapp->activeWindow());
+}
+
+void OutputPlugin::slotPreviewDVDXine()
+{
+  play(m_xine);
+}
+
+void OutputPlugin::slotPreviewDVDKaffeine()
+{
+  play(m_kaffeine);
+}
+
+void OutputPlugin::slotDVDInfo()
+{
+#ifdef HAVE_LIBDVDREAD
+  QString projectDir = projectInterface()->projectDir();
+  DVDInfo dlg(kapp->activeWindow(), projectDir + "DVD/");
+
+  dlg.exec();
+#endif
+}
+
+QStringList OutputPlugin::supportedProjectTypes()
+{
+  QStringList result;
+  result << "DVD-PAL" << "DVD-NTSC";
+  return result;
+}
+
+#include "outputplugin.moc"
---
+++ tags/release-0.6.0/src/kmficonview.h
@@ -0,0 +1,66 @@
+//**************************************************************************
+//   Copyright (C) 2004-2006 by Petri Damsten
+//   petri.damsten@iki.fi
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the
+//   Free Software Foundation, Inc.,
+//   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//**************************************************************************
+#ifndef KMFTEMPLATEICONVIEW_H
+#define KMFTEMPLATEICONVIEW_H
+
+#include <kmediafactory/kmfobject.h>
+#include "kmflistmodel.h"
+#include <klocale.h>
+#include <QItemDelegate>
+
+class KMFItemDelegate : public QItemDelegate
+{
+    Q_OBJECT
+  public:
+    virtual void paint(QPainter* painter, const QStyleOptionViewItem& option,
+                       const QModelIndex& index) const;
+    virtual QSize sizeHint(const QStyleOptionViewItem& option,
+                           const QModelIndex& index) const;
+  protected:
+    QString replaceNewLine(QString text) const;
+    QRect textLayoutBounds(const QStyleOptionViewItemV2 &option) const;
+};
+
+template <class T>
+class KMFObjectListModel : public KMFListModel<T>
+{
+  public:
+    QVariant data(const QModelIndex &index, int role) const
+    {
+      if(!KMFListModel<T>::isValid(index))
+        return QVariant();
+
+      if (role == Qt::DisplayRole)
+        return KMFListModel<T>::at(index)->title();
+      else if (role == Qt::DecorationRole)
+        return KMFListModel<T>::at(index)->pixmap();
+      return QVariant();
+    }
+
+    QVariant headerData(int, Qt::Orientation, int role) const
+    {
+      if (role != Qt::DisplayRole)
+        return QVariant();
+
+      return ::i18n("Title");
+    }
+};
+
+#endif // KMFTEMPLATEICONVIEW_H
