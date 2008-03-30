79d9f96a7fe3e2f3097436f16517fc07abda7039
petri.damsten <petri.damsten@4bd9ec94-6b1d-0410-bfb7-312270ef6092>
2008-03-30 17:02:56 +0000

    add some class templates

--- trunk/plugins/kross/krossmediaobject.h
+++ trunk/plugins/kross/krossmediaobject.h
@@ -1,5 +1,5 @@
 //**************************************************************************
-//   Copyright (C) 2008 by Petri Damsten <damu@iki.fi>
+//   Copyright (C) 2008 Petri Damsten <damu@iki.fi>
 //
 //   This program is free software; you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
@@ -16,3 +16,21 @@
 //   Free Software Foundation, Inc.,
 //   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //**************************************************************************
+#ifndef KROSSMEDIAOBJECT_H
+#define KROSSMEDIAOBJECT_H
+
+#include <mediaobject.h>
+
+/**
+	@author 
+*/
+class KrossMediaObject : public KMF::MediaObject
+{
+public:
+    KrossMediaObject();
+
+    ~KrossMediaObject();
+
+};
+
+#endif // KROSSMEDIAOBJECT_H
