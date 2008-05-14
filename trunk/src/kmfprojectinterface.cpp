b82e02bcc3d786ad9d284f42f6e3a9ec5c376a9c
petri.damsten@gmail.com <petri.damsten@gmail.com@4bd9ec94-6b1d-0410-bfb7-312270ef6092>
2008-05-14 07:54:30 +0000

    only one interface

--- trunk/src/kmfprojectinterface.cpp
+++ trunk/src/kmfprojectinterface.cpp
@@ -1,5 +1,5 @@
 //**************************************************************************
-//   Copyright (C) 2004-2006 by Petri Damsten
+//   Copyright (C) 2004-2008 by Petri Damsten
 //   petri.damsten@iki.fi
 //
 //   This program is free software; you can redistribute it and/or modify
@@ -17,6 +17,7 @@
 //   Free Software Foundation, Inc.,
 //   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //**************************************************************************
+
 #include "kmfprojectinterface.h"
 #include "kmediafactory.h"
 #include "templatepage.h"
@@ -26,16 +27,305 @@
 #include "kmfproject.h"
 #include <kdebug.h>
 
-KMFProjectInterface::KMFProjectInterface(QObject *parent) :
+KMFProgressDialog::KMFProgressDialog(QWidget* parent) : KMF::ProgressDialog(parent), m_pdlg(parent)
+{
+    m_pdlg.setAutoClose(false);
+}
+
+void KMFProgressDialog::setMaximum(int maximum)
+{
+    m_pdlg.progressBar()->setMaximum(maximum);
+}
+
+void KMFProgressDialog::setValue(int value)
+{
+    m_pdlg.progressBar()->setValue(value);
+}
+
+void KMFProgressDialog::setCaption(const QString &caption)
+{
+    m_pdlg.setCaption(caption);
+}
+
+void KMFProgressDialog::setLabel(const QString &label)
+{
+    m_pdlg.setLabelText(label);
+}
+
+void KMFProgressDialog::showCancelButton(bool show)
+{
+    m_pdlg.showCancelButton(show);
+}
+
+bool KMFProgressDialog::wasCancelled()
+{
+    return m_pdlg.wasCancelled();
+}
+
+void KMFProgressDialog::close()
+{
+    m_pdlg.close();
+    deleteLater();
+}
+
+KMFPluginInterface::KMFPluginInterface(QObject *parent) :
+  KMF::UiInterface(parent), m_useMessageBox(false), m_pdlg(0)
+{
+}
+
+KMFPluginInterface::~KMFPluginInterface()
+{
+}
+
+bool KMFPluginInterface::addMediaAction(QAction* action,
+                                    const QString& group) const
+{
+  kmfApp->mainWindow()->mediaPage->mediaButtons->add(action, group);
+  return true;
+}
+
+bool KMFPluginInterface::addMediaObject(KMF::MediaObject *media) const
+{
+  KMFProject* project = kmfApp->project();
+
+  if(project)
+    project->addItem(media);
+  return true;
+}
+
+bool KMFPluginInterface::addTemplateObject(KMF::TemplateObject* tob)
+{
+  KMFProject* project = kmfApp->project();
+
+  if(project)
+    project->templateObjects()->append(tob);
+  return true;
+}
+
+bool KMFPluginInterface::addOutputObject(KMF::OutputObject* oob)
+{
+  KMFProject* project = kmfApp->project();
+
+  if(project)
+    project->outputObjects()->append(oob);
+  return true;
+}
+
+bool KMFPluginInterface::removeMediaObject(KMF::MediaObject *media) const
+{
+  KMFProject* project = kmfApp->project();
+
+  if(project)
+    project->removeItem(media);
+  return true;
+}
+
+bool KMFPluginInterface::removeTemplateObject(KMF::TemplateObject* tob)
+{
+  KMFProject* project = kmfApp->project();
+
+  if(project)
+    project->templateObjects()->removeAll(tob);
+  return true;
+}
+
+bool KMFPluginInterface::removeOutputObject(KMF::OutputObject* oob)
+{
+  KMFProject* project = kmfApp->project();
+
+  if(project)
+    project->outputObjects()->removeAll(oob);
+  return true;
+}
+
+void KMFPluginInterface::addMediaObject(const QString& xml)
+{
+  kDebug() << xml;
+  QDomDocument doc;
+  doc.setContent(xml);
+  kmfApp->project()->mediaObjFromXML(doc.documentElement());
+}
+
+void KMFPluginInterface::selectTemplate(const QString& xml)
+{
+  QDomDocument doc;
+
+  doc.setContent(xml);
+  kmfApp->project()->templateFromXML(doc.documentElement());
+}
+
+void KMFPluginInterface::selectOutput(const QString& xml)
+{
+  QDomDocument doc;
+
+  doc.setContent(xml);
+  kmfApp->project()->outputFromXML(doc.documentElement());
+}
+
+bool KMFPluginInterface::message(KMF::MsgType type, const QString& msg)
+{
+  KMediaFactory* mainWindow = kmfApp->mainWindow();
+  QString pixmap;
+  QColor color;
+  KMessageBox::DialogType dlgType = KMessageBox::Information;
+
+  switch(type)
+  {
+    case KMF::None:
+    case KMF::Info:
+      pixmap = "dialog-information";
+      color = QColor("darkGreen");
+      dlgType = KMessageBox::Information;
+      break;
+    case KMF::Warning:
+      pixmap = "dialog-warning";
+      color = QColor(211, 183, 98);
+      dlgType = KMessageBox::Sorry;
+      break;
+    case KMF::Error:
+      pixmap = "dialog-error";
+      color = QColor("red");
+      dlgType = KMessageBox::Error;
+      break;
+    case KMF::OK:
+      pixmap = "dialog-ok";
+      color = QColor("darkGreen");
+      dlgType = KMessageBox::Information;
+      break;
+  }
+  setItemTotalSteps(0);
+  QListView* lv = mainWindow->outputPage->progressListView;
+  KMFProgressItemModel* model = static_cast<KMFProgressItemModel*>(lv->model());
+  KMFProgressItem item;
+  item.text = msg;
+  item.pixmap = pixmap;
+  model->append(item);
+  lv->scrollTo(model->lastIndex());
+
+  kmfApp->logger().message(msg, color);
+  if(m_useMessageBox)
+    KMessageBox::messageBox(mainWindow, dlgType, msg);
+  kmfApp->processEvents(QEventLoop::AllEvents);
+  return m_stopped;
+}
+
+KMF::Logger* KMFPluginInterface::logger()
+{
+  return &kmfApp->logger();
+}
+
+bool KMFPluginInterface::progress(int advance)
+{
+  KMediaFactory* mainWindow =
+      kmfApp->mainWindow();
+
+  if(advance)
+  {
+    /*
+    kDebug()
+        << mainWindow->outputPage->progressBar->value() << " + " << advance
+        << " / " << mainWindow->outputPage->progressBar->maximum()
+       ;
+    */
+    mainWindow->outputPage->progressBar->setValue(
+        mainWindow->outputPage->progressBar->value()+advance);
+  }
+  kmfApp->processEvents(QEventLoop::AllEvents);
+  return m_stopped;
+}
+
+bool KMFPluginInterface::setItemTotalSteps(int totalSteps)
+{
+  QListView* lv = kmfApp->mainWindow()->outputPage->progressListView;
+  KMFProgressItemModel* model = static_cast<KMFProgressItemModel*>(lv->model());
+  int last = model->rowCount() - 1;
+  KMFProgressItem item = model->at(last);
+  if(item.max != totalSteps)
+  {
+    item.max = totalSteps;
+    model->replace(last, item);
+  }
+  return m_stopped;
+}
+
+bool KMFPluginInterface::setItemProgress(int progress)
+{
+  QListView* lv = kmfApp->mainWindow()->outputPage->progressListView;
+  KMFProgressItemModel* model = static_cast<KMFProgressItemModel*>(lv->model());
+  int last = model->rowCount() - 1;
+  KMFProgressItem item = model->at(last);
+  if(item.value != progress)
+  {
+    item.value = progress;
+    model->replace(last, item);
+  }
+  return m_stopped;
+}
+
+void KMFPluginInterface::progressDialogDestroyed()
+{
+  m_pdlg = 0;
+}
+
+KMF::ProgressDialog* KMFPluginInterface::progressDialog(const QString &caption, const QString &label,
+                                                    int maximum)
+{
+  if(m_pdlg)
+    delete m_pdlg;
+  m_pdlg = new KMFProgressDialog(kmfApp->mainWindow());
+  connect(m_pdlg, SIGNAL(destroyed()), this, SLOT(progressDialogDestroyed()));
+  m_pdlg->setCaption(caption);
+  m_pdlg->setLabel(label);
+  m_pdlg->setMaximum(maximum);
+  m_pdlg->showCancelButton(true);
+  return m_pdlg;
+}
+
+KMF::ProgressDialog* KMFPluginInterface::progressDialog()
+{
+  if(m_pdlg)
+    return m_pdlg;
+  return progressDialog("KMediaFactory", i18n("Progress:"), 100);
+}
+
+int KMFPluginInterface::messageBox(const QString &caption, const QString &txt,
+                               int type)
+{
+  kDebug() << caption << txt << type <<KMessageBox::Error ;
+  if(type == KMessageBox::Information)
+    KMessageBox::information(kmfApp->mainWindow(), txt, caption);
+  else if(type == KMessageBox::Sorry)
+    KMessageBox::sorry(kmfApp->mainWindow(), txt, caption);
+  else if(type == KMessageBox::Error)
+    KMessageBox::error(kmfApp->mainWindow(), txt, caption);
+  return 0;
+}
+
+QStringList KMFPluginInterface::getOpenFileNames(const QString &startDir,
+                                             const QString &filter,
+                                             const QString &caption)
+{
+  QString start = QString("kfiledialog:///<%1>").arg(startDir);
+  //kDebug() << start;
+  return KFileDialog::getOpenFileNames(KUrl(start), filter,
+                                       kmfApp->mainWindow(), caption);
+}
+
+void KMFPluginInterface::debug(const QString &txt)
+{
+  kDebug() << txt;
+}
+
+KMFPluginInterface::KMFPluginInterface(QObject *parent) :
   KMF::ProjectInterface(parent)
 {
 }
 
-KMFProjectInterface::~KMFProjectInterface()
+KMFPluginInterface::~KMFPluginInterface()
 {
 }
 
-QString KMFProjectInterface::title()
+QString KMFPluginInterface::title()
 {
   if (kmfApp->project()) {
     return kmfApp->project()->title();
@@ -43,14 +333,14 @@ QString KMFProjectInterface::title()
   return QString();
 }
 
-void KMFProjectInterface::setTitle(QString title)
+void KMFPluginInterface::setTitle(QString title)
 {
   if (kmfApp->project()) {
     kmfApp->project()->setTitle(title);
   }
 }
 
-QList<KMF::MediaObject*> KMFProjectInterface::mediaObjects()
+QList<KMF::MediaObject*> KMFPluginInterface::mediaObjects()
 {
   if (kmfApp->project()) {
     return kmfApp->project()->mediaObjects()->list();
@@ -58,7 +348,7 @@ QList<KMF::MediaObject*> KMFProjectInterface::mediaObjects()
   return QList<KMF::MediaObject*>();
 }
 
-KMF::TemplateObject* KMFProjectInterface::templateObject()
+KMF::TemplateObject* KMFPluginInterface::templateObject()
 {
   if (kmfApp->project()) {
     return kmfApp->project()->templateObj();
@@ -66,7 +356,7 @@ KMF::TemplateObject* KMFProjectInterface::templateObject()
   return 0;
 }
 
-KMF::OutputObject* KMFProjectInterface::outputObject()
+KMF::OutputObject* KMFPluginInterface::outputObject()
 {
   if (kmfApp->project()) {
     return kmfApp->project()->output();
@@ -74,7 +364,7 @@ KMF::OutputObject* KMFProjectInterface::outputObject()
   return 0;
 }
 
-QString KMFProjectInterface::projectDir(const QString& subDir)
+QString KMFPluginInterface::projectDir(const QString& subDir)
 {
   if (kmfApp->project()) {
     return kmfApp->project()->directory(subDir);
@@ -82,7 +372,7 @@ QString KMFProjectInterface::projectDir(const QString& subDir)
   return QString();
 }
 
-void KMFProjectInterface::cleanFiles(const QString& subDir,
+void KMFPluginInterface::cleanFiles(const QString& subDir,
                                      const QStringList& files)
 {
   if (kmfApp->project()) {
@@ -90,14 +380,14 @@ void KMFProjectInterface::cleanFiles(const QString& subDir,
   }
 }
 
-void KMFProjectInterface::setDirty(KMF::ProjectInterface::DirtyType type)
+void KMFPluginInterface::setDirty(KMF::ProjectInterface::DirtyType type)
 {
   if (kmfApp->project()) {
     kmfApp->project()->setDirty(type);
   }
 }
 
-QString KMFProjectInterface::type()
+QString KMFPluginInterface::type()
 {
   if (kmfApp->project()) {
     return kmfApp->project()->type();
@@ -105,7 +395,7 @@ QString KMFProjectInterface::type()
   return QString();
 }
 
-QString KMFProjectInterface::lastSubType()
+QString KMFPluginInterface::lastSubType()
 {
   if (kmfApp->project()) {
     return kmfApp->project()->lastSubType();
@@ -113,7 +403,7 @@ QString KMFProjectInterface::lastSubType()
   return QString();
 }
 
-QDateTime KMFProjectInterface::lastModified(
+QDateTime KMFPluginInterface::lastModified(
     KMF::ProjectInterface::DirtyType type)
 {
   if (kmfApp->project()) {
@@ -122,7 +412,7 @@ QDateTime KMFProjectInterface::lastModified(
   return QDateTime();
 }
 
-int KMFProjectInterface::serial()
+int KMFPluginInterface::serial()
 {
   if (kmfApp->project()) {
     return kmfApp->project()->serial();
--- trunk/src/kmfprojectinterface.h
+++ trunk/src/kmfprojectinterface.h
@@ -1,5 +1,5 @@
 //**************************************************************************
-//   Copyright (C) 2004-2006 by Petri Damsten
+//   Copyright (C) 2004-2008 by Petri Damsten
 //   petri.damsten@iki.fi
 //
 //   This program is free software; you can redistribute it and/or modify
@@ -17,21 +17,38 @@
 //   Free Software Foundation, Inc.,
 //   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //**************************************************************************
-#ifndef KMFPROJECTINTERFACE_H
-#define KMFPROJECTINTERFACE_H
+
+#ifndef KMFPLUGININTERFACE_H
+#define KMFPLUGININTERFACE_H
 
 #include <kmediafactory/uiinterface.h>
 #include <kmediafactory/projectinterface.h>
 
-/**
-@author Petri Damsten
-*/
-class KMFProjectInterface : public KMF::ProjectInterface
+class KMFProgressDialog : public KMF::ProgressDialog
+{
+    Q_OBJECT
+  public: 
+    KMFProgressDialog(QWidget* parent);
+
+  public slots:
+    virtual void setMaximum(int maximum);
+    virtual void setValue(int value);
+    virtual void setCaption(const QString &caption);
+    virtual void setLabel(const QString &label);
+    virtual void showCancelButton(bool show);
+    virtual bool wasCancelled();
+    virtual void close();
+
+  private:
+    KProgressDialog m_pdlg;
+};
+
+class KMFPluginInterface : public KMF::PluginInterface
 {
     Q_OBJECT
   public:
-    KMFProjectInterface(QObject *parent = 0);
-    virtual ~KMFProjectInterface();
+    KMFPluginInterface(QObject *parent = 0);
+    virtual ~KMFPluginInterface();
 
     virtual QList<KMF::MediaObject*> mediaObjects();
     virtual KMF::TemplateObject* templateObject();
@@ -41,11 +58,49 @@ class KMFProjectInterface : public KMF::ProjectInterface
     virtual QString projectDir(const QString& subDir = "");
     virtual void cleanFiles(const QString& subDir,
                             const QStringList& files);
-    virtual void setDirty(KMF::ProjectInterface::DirtyType type);
+    virtual void setDirty(KMF::PluginInterface::DirtyType type);
     virtual QString type();
     virtual QString lastSubType();
-    virtual QDateTime lastModified(KMF::ProjectInterface::DirtyType type);
+    virtual QDateTime lastModified(KMF::PluginInterface::DirtyType type);
     virtual int serial();
+    virtual bool addMediaAction(QAction* action,
+                                const QString& group = "") const;
+    virtual bool addMediaObject(KMF::MediaObject* media) const;
+    virtual bool addTemplateObject(KMF::TemplateObject* tob);
+    virtual bool addOutputObject(KMF::OutputObject* oob);
+    virtual bool removeMediaObject(KMF::MediaObject* media) const;
+    virtual bool removeTemplateObject(KMF::TemplateObject* tob);
+    virtual bool removeOutputObject(KMF::OutputObject* oob);
+    virtual void addMediaObject(const QString& xml);
+    virtual void selectTemplate(const QString& xml);
+    virtual void selectOutput(const QString& xml);
+
+    virtual bool message(KMF::MsgType type, const QString& msg);
+    virtual bool progress(int advance);
+    virtual bool setItemTotalSteps(int totalSteps);
+    virtual bool setItemProgress(int progress);
+    virtual KMF::Logger* logger();
+    void setUseMessageBox(bool useMessageBox) { m_useMessageBox = useMessageBox; };
+    void setStopped(bool stopped) { m_stopped = stopped; };
+
+    // Plugin helpers
+    virtual QStringList getOpenFileNames(const QString &startDir,
+                                         const QString &filter,
+                                         const QString &caption);
+    virtual void debug(const QString &txt);
+    virtual int  messageBox(const QString &caption, const QString &txt,
+                            int type);
+    virtual KMF::ProgressDialog* progressDialog(const QString &caption, const QString &label,
+                                                int maximum);
+    virtual KMF::ProgressDialog* progressDialog();
+
+  public slots:
+    void progressDialogDestroyed();
+
+  private:
+    bool m_useMessageBox;
+    bool m_stopped;
+    KMF::ProgressDialog* m_pdlg;
 };
 
 #endif
