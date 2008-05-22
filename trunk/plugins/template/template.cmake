91d4111cb00429e1e00d296361fc071d0ef537cd
petri.damsten@gmail.com <petri.damsten@gmail.com@4bd9ec94-6b1d-0410-bfb7-312270ef6092>
2008-05-22 11:48:43 +0000

    Supress template making output

--- trunk/plugins/template/template.cmake
+++ trunk/plugins/template/template.cmake
@@ -1,19 +1,25 @@
 find_program(MSGFMT_EXECUTABLE msgfmt)
 find_program(ZIP_EXECUTABLE zip)
 
-MESSAGE(STATUS ${template_files})
+string(REGEX MATCHALL "([a-z/.]+)" files ${template_files})
 
-EXEC_PROGRAM(${ZIP_EXECUTABLE} ${src}
-    ARGS -X ${dest}/${template} ${template_files})
-FILE(MAKE_DIRECTORY locale)
-FILE(GLOB dirs RELATIVE "${src}/locale/" "${src}/locale/??")
-FOREACH(dir ${dirs})
-  FILE(MAKE_DIRECTORY locale/${dir})
-  FILE(MAKE_DIRECTORY locale/${dir}/LC_MESSAGES)
-  EXEC_PROGRAM(${MSGFMT_EXECUTABLE}
-      ARGS ${src}/locale/${dir}/LC_MESSAGES/template.po -o ${dest}/locale/${dir}/LC_MESSAGES/template.mo)
-  EXEC_PROGRAM(${MSGFMT_EXECUTABLE}
-      ARGS ${src}/locale/${dir}/LC_MESSAGES/ui.po -o ${dest}/locale/${dir}/LC_MESSAGES/ui.mo)
-ENDFOREACH(dir)
-EXEC_PROGRAM(${ZIP_EXECUTABLE} ${dest}
-    ARGS -X ${dest}/${template} -r locale)
+execute_process(WORKING_DIRECTORY ${src}
+                COMMAND ${ZIP_EXECUTABLE} -X ${dest}/${template} ${files}
+                OUTPUT_QUIET)
+file(MAKE_DIRECTORY locale)
+file(GLOB dirs RELATIVE "${src}/locale/" "${src}/locale/??")
+foreach(dir ${dirs})
+  file(MAKE_DIRECTORY locale/${dir})
+  file(MAKE_DIRECTORY locale/${dir}/LC_MESSAGES)
+  execute_process(COMMAND ${MSGFMT_EXECUTABLE}
+                          ${src}/locale/${dir}/LC_MESSAGES/template.po 
+                          -o ${dest}/locale/${dir}/LC_MESSAGES/template.mo
+                  OUTPUT_QUIET)
+  execute_process(COMMAND ${MSGFMT_EXECUTABLE}
+                          ${src}/locale/${dir}/LC_MESSAGES/ui.po 
+                          -o ${dest}/locale/${dir}/LC_MESSAGES/ui.mo
+                  OUTPUT_QUIET)
+endforeach(dir)
+execute_process(WORKING_DIRECTORY ${dest}
+                COMMAND ${ZIP_EXECUTABLE} -X ${dest}/${template} -r locale
+                OUTPUT_QUIET)
