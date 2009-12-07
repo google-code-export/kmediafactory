3177cbf8edce8e23886698b216e199a7ec54f0c4
petri.damsten@gmail.com <petri.damsten@gmail.com@4bd9ec94-6b1d-0410-bfb7-312270ef6092>
2009-12-07 14:16:21 +0000

    Tagging the 0.7.3 release.

---
+++ tags/release-0.7.3/lib/dvdauthorparser.cpp
@@ -0,0 +1,59 @@
+//**************************************************************************
+//   Copyright (C) 2004-2006 by Petri Damsten
+//   petri.damsten@iki.fi
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the
+//   Free Software Foundation, Inc.,
+//   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//**************************************************************************
+
+#include "dvdauthorparser.h"
+#include <kmftools.h>
+
+void KMF::DVDAuthorParser::setFile(const QString& fileName)
+{
+  m_file = fileName;
+  m_files.clear();
+  QString s;
+  if (KMF::Tools::loadStringFromFile(fileName, &s))
+    m_doc.setContent(s);
+}
+
+QStringList KMF::DVDAuthorParser::files()
+{
+  if(m_files.count() == 0)
+  {
+    QDomElement element = m_doc.documentElement();
+    findFiles(element);
+  }
+  return m_files;
+}
+
+void KMF::DVDAuthorParser::findFiles(const QDomElement& element)
+{
+  QDomNode n = element.firstChild();
+  while(!n.isNull())
+  {
+    QDomElement e = n.toElement();
+    if(e.tagName() == "vob")
+    {
+      m_files.append(e.attribute("file"));
+    }
+    else
+      findFiles(e);
+    n = n.nextSibling();
+  }
+}
+
+
---
+++ tags/release-0.7.3/lib/kmffontchooser.h
@@ -0,0 +1,60 @@
+//**************************************************************************
+//   Copyright (C) 2004-2006 by Petri Damsten
+//   petri.damsten@iki.fi
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the
+//   Free Software Foundation, Inc.,
+//   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//**************************************************************************
+#ifndef KMFFONTCHOOSER_H
+#define KMFFONTCHOOSER_H
+
+#include <kdemacros.h>
+#include <QWidget>
+#include <QLabel>
+
+class QLabel;
+class QPushButton;
+class QFont;
+
+/**
+	@author Petri Damsten <petri.damsten@iki.fi>
+*/
+
+class KDE_EXPORT KMFFontChooser : public QWidget
+{
+    Q_OBJECT
+    Q_PROPERTY(QFont font READ font WRITE setFont USER true)
+  public:
+    KMFFontChooser(QWidget *parent = 0);
+    ~KMFFontChooser();
+
+    QFont font() const { return m_font; };
+
+  public slots:
+    void setFont(const QFont& font);
+
+  protected:
+    void updateFontLabel();
+
+  protected slots:
+    void buttonClicked();
+
+  private:
+    QFont m_font;
+    QLabel* m_label;
+    QPushButton* m_button;
+};
+
+#endif
