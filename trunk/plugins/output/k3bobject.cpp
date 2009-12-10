426538d976cfa886e0885a048a5663c5781cf019
petri.damsten@gmail.com <petri.damsten@gmail.com@4bd9ec94-6b1d-0410-bfb7-312270ef6092>
2009-12-10 18:48:03 +0000

    KRun does not seem to be thread safe

--- trunk/plugins/output/k3bobject.cpp
+++ trunk/plugins/output/k3bobject.cpp
@@ -29,10 +29,12 @@
 #include <KIconLoader>
 #include <KUrl>
 #include <KRun>
+#include <KDebug>
 #include <QDir>
 #include <QPixmap>
 #include <QDomDocument>
 #include <QTextStream>
+#include <QWidget>
 
 static const char startString[] = I18N_NOOP("K3b project");
 
@@ -48,7 +50,13 @@ public:
     if (!cmd.isEmpty())
     {
       cmd += " \"" + doc + "\"";
-      KRun::runCommand(cmd, kapp->activeWindow());
+      // KRun does not seem to be thread safe
+      //KRun::runCommand(cmd, kapp->activeWindow());
+      QProcess *process;
+      process = new QProcess;
+      process->start(cmd);
+      connect(process, SIGNAL(finished(int, QProcess::ExitStatus)),
+              process, SLOT(deleteLater()));
     }
     message(msgId(), KMF::Done);
   }
@@ -69,7 +77,7 @@ public:
   }
 
   // All the following are taken from k3b project
-  
+
   bool saveDocument(const KUrl& url)
   {
     // create the store
@@ -77,24 +85,24 @@ public:
                                           "application/x-k3b");
     if(!store)
       return false;
-  
+
     if(store->bad())
     {
       delete store;
       return false;
     }
-  
+
     // open the document inside the store
     store->open("maindata.xml");
-  
+
     // save the data in the document
     QDomDocument xmlDoc("k3b_video_dvd_project");
-  
+
     xmlDoc.appendChild(xmlDoc.createProcessingInstruction( "xml",
                       "version=\"1.0\" encoding=\"UTF-8\""));
     QDomElement docElem = xmlDoc.createElement("k3b_video_dvd_project");
     xmlDoc.appendChild(docElem);
-  
+
     bool success = saveDocumentData(&docElem);
     if(success)
     {
@@ -104,44 +112,44 @@ public:
       xmlStream.setCodec("UTF-8");
       xmlDoc.save(xmlStream, 0);
     }
-  
+
     // close the document inside the store
     store->close();
-  
+
     // remove the store (destructor writes the store to disk)
     delete store;
     return success;
   }
-  
+
   bool saveDocumentData(QDomElement* docElem)
   {
     QDomDocument doc = docElem->ownerDocument();
-  
+
     saveGeneralDocumentData(docElem);
-  
+
     // all options
     // ----------------------------------------------------------------------
     QDomElement optionsElem = doc.createElement("options");
     saveDocumentDataOptions(optionsElem);
     docElem->appendChild(optionsElem);
     // ----------------------------------------------------------------------
-  
+
     // the header stuff
     // ----------------------------------------------------------------------
     QDomElement headerElem = doc.createElement("header");
     saveDocumentDataHeader(headerElem);
     docElem->appendChild(headerElem);
-  
+
     // now do the "real" work: save the entries
     // ----------------------------------------------------------------------
     QDomElement topElem = doc.createElement("files");
     addFiles(dvdDir, &doc, &topElem);
     docElem->appendChild(topElem);
     // ----------------------------------------------------------------------
-  
+
     return true;
   }
-  
+
   bool saveGeneralDocumentData(QDomElement* part)
   {
     QDomDocument doc = part->ownerDocument();
@@ -163,28 +171,28 @@ public:
         break;
     }
     mainElem.appendChild( propElem );
-  
+
     propElem = doc.createElement( "dummy" );
     propElem.setAttribute( "activated", dummy() ? "yes" : "no" );
     mainElem.appendChild( propElem );
-  
+
     propElem = doc.createElement( "on_the_fly" );
     propElem.setAttribute( "activated", onTheFly() ? "yes" : "no" );
     mainElem.appendChild( propElem );
-  
+
     propElem = doc.createElement( "only_create_images" );
     propElem.setAttribute( "activated", onlyCreateImages() ? "yes" : "no" );
     mainElem.appendChild( propElem );
-  
+
     propElem = doc.createElement( "remove_images" );
     propElem.setAttribute( "activated", removeImages() ? "yes" : "no" );
     mainElem.appendChild( propElem );
     */
     part->appendChild( mainElem );
-  
+
     return true;
   }
-  
+
   void saveDocumentDataOptions(QDomElement&)
   {
     /*
@@ -193,111 +201,111 @@ public:
     topElem.setAttribute( "activated", isoOptions().createRockRidge() ? "yes" :
         "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "joliet" );
     topElem.setAttribute( "activated", isoOptions().createJoliet() ? "yes" : "no"
                         );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "udf" );
     topElem.setAttribute( "activated", isoOptions().createUdf() ? "yes" : "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "iso_allow_lowercase" );
     topElem.setAttribute( "activated", isoOptions().ISOallowLowercase() ? "yes" :
         "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "iso_allow_period_at_begin" );
     topElem.setAttribute( "activated", isoOptions().ISOallowPeriodAtBegin() ?
         "yes" : "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "iso_allow_31_char" );
     topElem.setAttribute( "activated", isoOptions().ISOallow31charFilenames() ?
         "yes" : "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "iso_omit_version_numbers" );
     topElem.setAttribute( "activated", isoOptions().ISOomitVersionNumbers() ?
         "yes" : "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "iso_omit_trailing_period" );
     topElem.setAttribute( "activated", isoOptions().ISOomitTrailingPeriod() ?
         "yes" : "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "iso_max_filename_length" );
     topElem.setAttribute( "activated", isoOptions().ISOmaxFilenameLength() ? "yes"
     : "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "iso_relaxed_filenames" );
     topElem.setAttribute( "activated", isoOptions().ISOrelaxedFilenames() ? "yes"
     : "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "iso_no_iso_translate" );
     topElem.setAttribute( "activated", isoOptions().ISOnoIsoTranslate() ? "yes" :
         "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "iso_allow_multidot" );
     topElem.setAttribute( "activated", isoOptions().ISOallowMultiDot() ? "yes" :
         "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "iso_untranslated_filenames" );
     topElem.setAttribute( "activated", isoOptions().ISOuntranslatedFilenames() ?
         "yes" : "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "follow_symbolic_links" );
     topElem.setAttribute( "activated", isoOptions().followSymbolicLinks() ? "yes"
     : "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "create_trans_tbl" );
     topElem.setAttribute( "activated", isoOptions().createTRANS_TBL() ? "yes" :
         "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "hide_trans_tbl" );
     topElem.setAttribute( "activated", isoOptions().hideTRANS_TBL() ? "yes" : "no"
                         );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "iso_level" );
     topElem.appendChild( doc.createTextNode(
         QString::number(isoOptions().ISOLevel()) ) );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "discard_symlinks" );
     topElem.setAttribute( "activated", isoOptions().discardSymlinks() ? "yes" :
         "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "discard_broken_symlinks" );
     topElem.setAttribute( "activated", isoOptions().discardBrokenSymlinks() ?
         "yes" : "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "preserve_file_permissions" );
     topElem.setAttribute( "activated", isoOptions().preserveFilePermissions() ?
         "yes" : "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "force_input_charset" );
     topElem.setAttribute( "activated", isoOptions().forceInputCharset() ? "yes" :
         "no" );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "input_charset" );
     topElem.appendChild( doc.createTextNode( isoOptions().inputCharset() ) );
     optionsElem.appendChild( topElem );
-  
-  
+
+
     topElem = doc.createElement( "whitespace_treatment" );
     switch( isoOptions().whiteSpaceTreatment() ) {
       case K3bIsoOptions::strip:
@@ -314,12 +322,12 @@ public:
         break;
     }
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "whitespace_replace_string" );
     topElem.appendChild( doc.createTextNode(
         isoOptions().whiteSpaceTreatmentReplaceString() ) );
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "data_track_mode" );
     if( m_dataMode == K3b::MODE1 )
       topElem.appendChild( doc.createTextNode( "mode1" ) );
@@ -328,8 +336,8 @@ public:
     else
       topElem.appendChild( doc.createTextNode( "auto" ) );
     optionsElem.appendChild( topElem );
-  
-  
+
+
     // save multisession
     topElem = doc.createElement( "multisession" );
     switch( m_multisessionMode ) {
@@ -347,20 +355,20 @@ public:
       break;
     }
     optionsElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "verify_data" );
     topElem.setAttribute( "activated", verifyData() ? "yes" : "no" );
     optionsElem.appendChild( topElem );
     // ----------------------------------------------------------------------
     */
   }
-  
+
   void saveDocumentDataHeader(QDomElement& headerElem)
   {
     QString app = i18n("%1 - Version %2"
         , KGlobal::mainComponent().aboutData()->programName()
         , KGlobal::mainComponent().aboutData()->version());
-  
+
     QDomDocument doc = headerElem.ownerDocument();
     QDomElement topElem = doc.createElement("volume_id");
     topElem.appendChild(doc.createTextNode(title));
@@ -369,25 +377,25 @@ public:
     topElem = doc.createElement( "volume_set_id" );
     topElem.appendChild( doc.createTextNode( isoOptions().volumeSetId() ) );
     headerElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "volume_set_size" );
     topElem.appendChild( doc.createTextNode(
         QString::number(isoOptions().volumeSetSize()) ) );
     headerElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "volume_set_number" );
     topElem.appendChild( doc.createTextNode(
         QString::number(isoOptions().volumeSetNumber()) ) );
     headerElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "system_id" );
     topElem.appendChild( doc.createTextNode( isoOptions().systemId() ) );
     headerElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "application_id" );
     topElem.appendChild( doc.createTextNode( isoOptions().applicationID() ) );
     headerElem.appendChild( topElem );
-  
+
     topElem = doc.createElement( "publisher" );
     topElem.appendChild( doc.createTextNode( isoOptions().publisher() ) );
     headerElem.appendChild( topElem );
@@ -397,12 +405,12 @@ public:
     headerElem.appendChild(topElem);
     // ----------------------------------------------------------------------
   }
-  
+
   void addFiles(const QString& directory, QDomDocument* doc,
                           QDomElement* parent)
   {
     QDir dir(directory);
-  
+
     if(!dir.exists())
       return;
     const QFileInfoList& files = dir.entryInfoList();
@@ -410,11 +418,11 @@ public:
     {
       if(file.fileName() == "." || file.fileName() == "..")
         continue;
-  
+
       saveDataItem(&file, doc, parent);
     }
   }
-  
+
   void saveDataItem(const QFileInfo* fi, QDomDocument* doc,
                               QDomElement* parent)
   {
@@ -482,7 +490,7 @@ bool K3bObject::prepare(const QString& type)
   {
     interface()->message(msgId(), KMF::Info, i18n("K3b project is up to date"));
   }
-  else 
+  else
   {
     K3bProjectJob *job = new K3bProjectJob();
     job->doc = interface()->projectDir() + "dvd.k3b";
