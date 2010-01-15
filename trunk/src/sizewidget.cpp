ba9d349463e3c98f4612b4d24d075536db5f30ed
petri.damsten@gmail.com <petri.damsten@gmail.com@4bd9ec94-6b1d-0410-bfb7-312270ef6092>
2010-01-15 12:29:32 +0000

    One more try for size widget

--- trunk/src/sizewidget.cpp
+++ trunk/src/sizewidget.cpp
@@ -31,12 +31,9 @@
 
 
 SizeWidget::SizeWidget(QWidget* parent)
-  : KCapacityBar(KCapacityBar::DrawTextInline, parent)
+  : QWidget(parent)
+  , m_bar(KCapacityBar::DrawTextInline)
 {
-    m_barElement = KStyle::customControlElement("CE_CapacityBar", this);
-    if (!m_barElement) {
-        m_barElement = QStyle::CE_ProgressBar;
-    }
 }
 
 SizeWidget::~SizeWidget()
@@ -45,58 +42,66 @@ SizeWidget::~SizeWidget()
 
 void SizeWidget::paintEvent(QPaintEvent *ev)
 {
-    if (value() > 100) {
-        QStyleOptionProgressBar opt;
-        KColorScheme cs(QPalette::Active, KColorScheme::Window);
-        QColor color = cs.foreground(KColorScheme::NegativeText).color();
-        bool reverse = (Qt::RightToLeft == layoutDirection());
-        QRect rc = rect();
-        int v = 100 * rc.width() / value();
-        QPainter painter(this);
+    QPixmap barPixmap;
+    QPainter painter(this);
+    bool reverse = (Qt::RightToLeft == layoutDirection());
+    QRect rc = rect();
+    bool exceeded = (m_bar.value() > 100);
+    int savedValue = m_bar.value();
+    int v = qMin(savedValue, 100) * rc.width() / qMax(savedValue, 100);
 
-        opt.initFrom(this);
-        opt.rect = ev->rect();
-        opt.minimum = 0;
-        opt.textVisible = true;
-        opt.text = text();
-        opt.textAlignment = Qt::AlignCenter;
-        opt.maximum = 100;
-        opt.progress = 100;
+    if (exceeded) {
+        m_bar.setValue(100);
         painter.setClipRect(reverse ? v : rc.left(), rc.top(),
                             reverse ? rc.right() : v, rc.bottom());
-        style()->drawControl(m_barElement, &opt, &painter, this);
-        opt.palette.setColor(QPalette::Active, QPalette::Highlight, color);
+    }
+    barPixmap = QPixmap::grabWidget(&m_bar, ev->rect());
+    painter.drawPixmap(ev->rect().topLeft(), barPixmap);
+    if (exceeded) {
+        KColorScheme cs(QPalette::Active, KColorScheme::Window);
+        QColor color = cs.foreground(KColorScheme::NegativeText).color();
+        QPalette original = m_bar.palette();
+        QPalette palette = original;
+
+        palette.setColor(QPalette::Active, QPalette::Highlight, color);
+        m_bar.setPalette(palette);
+        barPixmap = QPixmap::grabWidget(&m_bar, ev->rect());
         painter.setClipRect(reverse ? rc.left() : v, rc.top(),
                             reverse ? v : rc.right(), rc.bottom());
-        style()->drawControl(m_barElement, &opt, &painter, this);
-        return;
+        painter.drawPixmap(ev->rect().topLeft(), barPixmap);
+        m_bar.setPalette(original);
+        m_bar.setValue(savedValue);
     }
-    KCapacityBar::paintEvent(ev);
 }
 
 void SizeWidget::setSizes(quint64 max, quint64 size)
 {
-    setValue(size * 100 / max);
+    m_bar.setValue(size * 100 / max);
     updateLabel(max, size);
     updateGeometry();
 }
 
 void SizeWidget::updateLabel(quint64 max, quint64 size)
 {
-    QFont f(font());
+    QFont f(m_bar.font());
     if (size > max) {
         f.setBold(true);
-        setText(i18n("Capacity (%1) exceeded by %2",
-                KGlobal::locale()->formatByteSize(max),
-                KGlobal::locale()->formatByteSize(size - max)));
+        m_bar.setText(i18n("Capacity (%1) exceeded by %2",
+                      KGlobal::locale()->formatByteSize(max),
+                      KGlobal::locale()->formatByteSize(size - max)));
     } else {
         f.setBold(false);
-        setText(i18n("%1 of %2 used (%3%)",
-                KGlobal::locale()->formatByteSize(size),
-                KGlobal::locale()->formatByteSize(max),
-                (100 * size) / max));
+        m_bar.setText(i18n("%1 of %2 used (%3%)",
+                      KGlobal::locale()->formatByteSize(size),
+                      KGlobal::locale()->formatByteSize(max),
+                      (100 * size) / max));
     }
-    setFont(f);
+    m_bar.setFont(f);
+}
+
+QSize SizeWidget::minimumSizeHint() const
+{
+    return m_bar.minimumSizeHint();
 }
 
 #include "sizewidget.moc"
